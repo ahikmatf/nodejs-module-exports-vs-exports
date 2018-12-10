209da25a75ddd916770c9b85f0664506191f623f
Asep Hikmat Fatahillah <asephikmatf@gmail.com>
2018-12-11 06:35:18 +0700

    Adding my two cents comment about module.exports

--- src/addition.js
+++ src/addition.js
@@ -16,3 +16,12 @@ module.exports.threeNumbers = threeNumbers
 console.log("-- START addition.js")
 console.log("this thing is printed when the module is 'require-ed'")
 console.log("-- END addition.js")
+
+/**
+ * if you are having 2 lines that says
+ *  module.exports = { twoNumbers }
+ *  module.exports = { threeNumbers }
+ * then the 'twoNumbers' won't be exported because it is overwritten, and if you do
+ *  module.exports = threeNumbers
+ * will also will not be exported, since you are overwriting the exports part of the module
+ */
